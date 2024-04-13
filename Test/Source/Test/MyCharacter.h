92a20af28b81a21081d63ed26142823a898a919e
fairy-jini <praesentia05@gmail.com>
2024-04-14 01:58:45 +0900

    Stat Manager

--- Test/Source/Test/MyCharacter.h
+++ Test/Source/Test/MyCharacter.h
@@ -38,6 +38,7 @@ public:
 	UFUNCTION()
 	void OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted);
 
+	virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser);
 private:
 	UPROPERTY(VisibleAnywhere)
 	class USpringArmComponent* SpringArm;
@@ -63,4 +64,7 @@ public:
 
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* Weapon;
+
+	UPROPERTY(VisibleAnywhere)
+	class UMyStatComponent* Stat;
 };
